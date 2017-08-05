1757ef361cc0bfe96b88a6b1bac94dc2f35ed549
AntumDeluge <antumdeluge@gmail.com>
2017-08-05 14:40:22 -0700

    Add initial code

---
+++ api.lua
@@ -0,0 +1,12 @@
+--[[ LICENSE HEADER
+  
+  MIT Licensing
+  
+  Copyright © 2017 Jordan Irwin (AntumDeluge)
+  
+  See: LICENSE.txt
+]]
+
+--- ***modconf*** mod API.
+--
+-- @script api.lua
---
+++ init.lua
@@ -0,0 +1,23 @@
+--[[ LICENSE HEADER
+  
+  MIT Licensing
+  
+  Copyright © 2017 Jordan Irwin (AntumDeluge)
+  
+  See: LICENSE.txt
+]]
+
+--- @script init.lua
+
+
+modconf = {}
+modconf.path = core.get_modpath(core.get_current_modname())
+
+
+local scripts = {
+	'api',
+}
+
+for i, s in ipairs(scripts) do
+	dofile(modconf.path .. '/' .. s .. '.lua')
+end
